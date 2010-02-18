88cb112c2b90c60e8b10d61be8a33ea1a0eee6e7
Christopher David Yudichak <TheOddLinguist@gmail.com>
2010-02-18 11:36:46 -0500

    added description to categories

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100211164034) do
+ActiveRecord::Schema.define(:version => 20100218163504) do
 
   create_table "categories", :force => true do |t|
     t.integer  "user_id"
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(:version => 20100211164034) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   create_table "items", :force => true do |t|
