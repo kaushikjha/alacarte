b00ca1e3eafe10fa9d78c17e780650bfc8ce3464
Christopher David Yudichak <TheOddLinguist@gmail.com>
2010-02-18 11:40:34 -0500

    added description to menus.  Closes #3

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100218163504) do
+ActiveRecord::Schema.define(:version => 20100218163708) do
 
   create_table "categories", :force => true do |t|
     t.integer  "user_id"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(:version => 20100218163504) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text     "description"
   end
 
   create_table "prices", :force => true do |t|
