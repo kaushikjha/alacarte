9c93c77daa87e91196087c3ad7c226ba80600299
Christopher David Yudichak <TheOddLinguist@gmail.com>
2010-02-19 10:52:36 -0500

    added acts_as_list and menu position migration

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100218163708) do
+ActiveRecord::Schema.define(:version => 20100219153841) do
 
   create_table "categories", :force => true do |t|
     t.integer  "user_id"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(:version => 20100218163708) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "description"
+    t.integer  "position"
   end
 
   create_table "prices", :force => true do |t|
