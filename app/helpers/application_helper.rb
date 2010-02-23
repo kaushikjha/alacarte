6d7ec06f48990d0fb1099d8658b5f23d906c9ebc
Christopher David Yudichak <TheOddLinguist@gmail.com>
2010-02-23 11:31:25 -0500

    updating link_to_remove_fields helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
-  def link_to_remove_fields(name, f)
-    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)")
+  def link_to_remove_fields(name, f, *args)
+    options = args.first || {}
+    f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)", options)
   end
   
   def link_to_add_fields(name, f, association)
