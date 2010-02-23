c21263ad05a9a8a3dbbed9fa3ea15a361c2c4e0e
Christopher David Yudichak <TheOddLinguist@gmail.com>
2010-02-23 11:42:41 -0500

    updated link_to_add_fields() function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,11 +5,12 @@ module ApplicationHelper
     f.hidden_field(:_destroy) + link_to_function(name, "remove_fields(this)", options)
   end
   
-  def link_to_add_fields(name, f, association)
+  def link_to_add_fields(name, f, association, *args)
+    options = args.first || {}
     new_object = f.object.class.reflect_on_association(association).klass.new
     fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
       render("shared/" + association.to_s.singularize + "_fields", :f => builder)
     end
-    link_to_function(name, h("add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")"))
+    link_to_function(name, h("add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")"), options)
   end
 end
