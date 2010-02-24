481b00ab2dd52e7ddfba3edf28dab48c19e66520
Christopher David Yudichak <TheOddLinguist@gmail.com>
2010-02-24 10:34:01 -0500

    restricting api auth fitler to xml and json requests

--- lib/api_authorized_filter.rb
+++ lib/api_authorized_filter.rb
@@ -1,30 +1,33 @@
 class ApiAuthorizedFilter
   def before(controller)
-    if @token = controller.params[:token]
-      if @user = User.find_by_single_access_token(@token)
-        if @user.hits < @user.rate_limit
-          @user.increment!(:hits)
-          @user_session = UserSession.new(User.find_by_single_access_token(@token))
-          @user_session.save
-          @token = true
-        else
-          error = Error["Rate Limited"]
-          respond_to do |format|
-            format.xml { render :xml => error, :status => 403 }
-            format.json { render :json => error, :status => 403 }
+    if controller.request.format.xml? || controller.request.format.json?
+      if @token = controller.params[:token]
+        if @user = User.find_by_single_access_token(@token)
+          if @user.hits < @user.rate_limit
+            @user.increment!(:hits)
+            @user_session = UserSession.new(User.find_by_single_access_token(@token))
+            @user_session.save
+          else
+            error = Error["Rate Limited"]
+            respond_to do |format|
+              format.xml { render :xml => error, :status => 403 }
+              format.json { render :json => error, :status => 403 }
+            end
           end
-        end
-      else
-        error = Error["Invalid Token"]
-        respond_to do |format|
-          format.xml { render :xml => error, :status => 401 }
-          format.json { render :json => error, :status => 401 }
+        else
+          error = Error["Invalid Token"]
+          respond_to do |format|
+            format.xml { render :xml => error, :status => 401 }
+            format.json { render :json => error, :status => 401 }
+          end
         end
       end
     end
   end
   
   def after(controller)
-    controller.sign_out!
+    if controller.request.format.xml? || controller.request.format.json?
+      controller.sign_out!
+    end
   end
 end
