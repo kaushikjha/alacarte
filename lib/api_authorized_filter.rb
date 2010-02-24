410714c2d068e1d4d81b7028cc842b373df2cdc9
Christopher David Yudichak <TheOddLinguist@gmail.com>
2010-02-24 10:45:53 -0500

    this is probably a better way to handle the api filter

--- lib/api_authorized_filter.rb
+++ lib/api_authorized_filter.rb
@@ -1,32 +1,30 @@
 class ApiAuthorizedFilter
   def before(controller)
-    if controller.request.format.xml? || controller.request.format.json?
-      if @token = controller.params[:token]
-        if @user = User.find_by_single_access_token(@token)
-          if @user.hits < @user.rate_limit
-            @user.increment!(:hits)
-            @user_session = UserSession.new(User.find_by_single_access_token(@token))
-            @user_session.save
-          else
-            error = Error["Rate Limited"]
-            respond_to do |format|
-              format.xml { render :xml => error, :status => 403 }
-              format.json { render :json => error, :status => 403 }
-            end
-          end
+    if @token = controller.params[:token]
+      if @user = User.find_by_single_access_token(@token)
+        if @user.hits < @user.rate_limit
+          @user.increment!(:hits)
+          @user_session = UserSession.new(User.find_by_single_access_token(@token))
+          @user_session.save
         else
-          error = Error["Invalid Token"]
+          error = Error["Rate Limited"]
           respond_to do |format|
-            format.xml { render :xml => error, :status => 401 }
-            format.json { render :json => error, :status => 401 }
+            format.xml { render :xml => error, :status => 403 }
+            format.json { render :json => error, :status => 403 }
           end
+        end
+      else
+        error = Error["Invalid Token"]
+        respond_to do |format|
+          format.xml { render :xml => error, :status => 401 }
+          format.json { render :json => error, :status => 401 }
         end
       end
     end
   end
   
   def after(controller)
-    if controller.request.format.xml? || controller.request.format.json?
+    if @token = controller.params[:token]
       controller.sign_out!
     end
   end
