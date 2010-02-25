161ed3c306abd2196e7199db00df52f737139cbe
Christopher David Yudichak <TheOddLinguist@gmail.com>
2010-02-25 12:43:28 -0500

    added feedback form and basic styling

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,2 +1,11 @@
 class FeedbackController < ApplicationController
+  def create
+    @feedback = Feedback.new(params[:feedback])
+    @feedback.user = current_user
+    @feedback.time = Time.now
+    if @feedback.save flash[:notice] = 'Your feedback has been recorded.  Thank you.'
+    else flash[:error] = 'There was a problem recording your feedback.  Please try again.'
+    end
+    redirect_to request.referer
+  end
 end
