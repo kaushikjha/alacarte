6b4abc30809c7b0bc14064660fdc3b8f324601c0
Christopher David Yudichak <TheOddLinguist@gmail.com>
2010-02-26 15:23:51 -0500

    minor styling additions to docs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,8 @@ class PagesController < ApplicationController
     else
       if %w(home help).include?(params[:id])
         render "pages/show/#{params[:id]}"
+      elsif params[:help_id]
+        render "pages/show/help/#{params[:help_id]}"
       else
         render :status => 404
       end
