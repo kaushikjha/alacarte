ffda34b9760f60247d8976df5bee900d70849462
Christopher David Yudichak <TheOddLinguist@gmail.com>
2010-03-08 10:43:59 -0500

    bugfix: moved constant declaration to after rails initialization

--- config/environment.rb
+++ config/environment.rb
@@ -2,12 +2,13 @@
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
-POSTMARK_API_KEY_FILE = File.join(RAILS_ROOT, '..', '..', 'shared', 'config', 'postmark_api_key')
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  POSTMARK_API_KEY_FILE = File.join(RAILS_ROOT, '..', '..', 'shared', 'config', 'postmark_api_key')
+  
   config.gem 'haml', :version => '>=2.2'
   config.gem "authlogic"
   config.gem "cancan"
