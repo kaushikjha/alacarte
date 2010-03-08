bf6796133ff779ed572571e2fcbcd833d4d09473
Christopher David Yudichak <TheOddLinguist@gmail.com>
2010-03-08 10:36:55 -0500

    experimenting with api key setup

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,7 @@
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+POSTMARK_API_KEY_FILE = File.join(RAILS_ROOT, '..', '..', 'shared', 'config', 'postmark_api_key')
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -15,7 +16,7 @@ Rails::Initializer.run do |config|
   
   require 'postmark-rails'
   
-  config.action_mailer.postmark_api_key = "POSTMARK_API_KEY"
+  config.action_mailer.postmark_api_key = File.read(POSTMARK_API_KEY_FILE).chomp if File.readable? POSTMARK_API_KEY_FILE
 
   config.time_zone = 'UTC'
 end
