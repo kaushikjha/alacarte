b683fab666fd691d3d51eea997088b4e4f4591b7
Christopher David Yudichak <TheOddLinguist@gmail.com>
2010-03-08 11:12:01 -0500

    updating mailer.rb

--- app/models/mailer.rb
+++ app/models/mailer.rb
@@ -4,7 +4,7 @@ class Mailer < ActionMailer::Base
   def invitation(invitation, signup_url)
     subject    "You've been invited to sign up for Alacarte"
     recipients invitation.recipient_email
-    from       'admin@example.com'
+    from       'support@alacarteapp.com'
     body       :invitation => invitation, :signup_url => signup_url
     invitation.update_attribute(:sent_at, Time.now)
   end
